fc0867b32c827a85c65a3c7867419221286d71f9
Mayur Naresh Gondhkar <mayurngondhkar@gmail.com>
2019-10-16 19:11:10 +1100

    Create migrations step table

---
+++ database/migrations/2019_10_16_080119_create_step_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStepTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('step', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('todolist_id');
+            $table->integer('order_in_todolist');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('step');
+    }
+}
