853ea5969201293713dd372bf51d2d024fb393f2
Mayur Naresh Gondhkar <mayurngondhkar@gmail.com>
2019-10-16 19:43:42 +1100

    Define 'Todolist' relation to 'Step'

--- app/todolist.php
+++ app/todolist.php
@@ -9,4 +9,8 @@ class Todolist extends Model
 {
     use SoftDeletes;
     protected $fillable = ['name', 'description', 'order'];
+
+    public function steps() {
+        return $this->hasMany('App/Step');
+    }
 }
