989704647ed7e2c44e1f82892fb534064030a231
Mayur Naresh Gondhkar <mayurngondhkar@gmail.com>
2019-10-20 08:55:58 +1100

    Create To Do List, Step and Task factories for seeders
    Additional data for testing

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,9 +11,43 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+//$factory->define(App\User::class, function (Faker\Generator $faker) {
+//    return [
+//        'name' => $faker->name,
+//        'email' => $faker->email,
+//    ];
+//});
+
+$factory->define(App\Todolist::class, function (Faker\Generator $faker) {
+    static $order = 1;
+    echo 1;
     return [
-        'name' => $faker->name,
-        'email' => $faker->email,
+        'name' => 'To Do List Name' . $faker->text(8),
+        'description' => 'To Do List Description' . $faker->text(20),
+        'order' => $order++,
+        'user_id' => $faker->numberBetween(1, 2),
+    ];
+});
+
+$factory->define(App\Step::class, function (Faker\Generator $faker) {
+    static $order = 1;
+    return [
+        'name' => 'Step Name ' . $faker->text(8),
+        'description' => 'Step Description ' . $faker->text(20),
+//        'todolist_id' => ,
+        'order_in_todolist' => $order++,
+//        'user_id' =>
+    ];
+});
+
+$factory->define(App\Task::class, function (Faker\Generator $faker) {
+    static $order = 1;
+    return [
+        'name' => 'Task Name ' . $faker->text(8),
+        'description' => 'Task Description ' . $faker->text(20),
+        'state_id' => $faker->numberBetween(0, 1),
+//        'step_id' => ,
+        'order_in_steplist' => $order++,
+//        'user_id' =>
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,16 @@ class DatabaseSeeder extends Seeder
         $this->call(StepsTableSeeder::class);
         $this->call(TasksTableSeeder::class);
         $this->call(StatesTableSeeder::class);
+
+        factory(App\Todolist::class, 4)
+            ->create()
+            ->each(function ($todolist) {
+                factory(App\Step::class, 4)
+                    ->create(['todolist_id' => $todolist['id'], 'user_id' => $todolist['user_id']])
+                    ->each(function ($step) {
+                        factory(App\Task::class, 4)
+                            ->create(['user_id' => $step['user_id'], 'step_id' => $step['id']]);
+                    });
+            });
     }
 }
